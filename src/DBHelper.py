5513237c23ed0e0f703c0cfee410c5f7062588e3
Trap Daddy <Trap Daddy>
2017-05-11 14:51:49 +0800

    Hot fix

--- src/DBHelper.py
+++ src/DBHelper.py
@@ -5,6 +5,7 @@ import config
 g_db_connection = pymysql.connect(**config.DATABASE['main'], cursorclass=pymysql.cursors.DictCursor)
 
 def get_db_conn():
+    global g_db_connection
     if g_db_connection is None or not g_db_connection.open:
         g_db_connection = pymysql.connect(**config.DATABASE['main'], cursorclass=pymysql.cursors.DictCursor)
     return g_db_connection
